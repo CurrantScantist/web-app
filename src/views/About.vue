e354d19d0378218b21f983a092ebfa3007c80c34
shourya <sraj0008@student.monash.edu>
2021-07-18 14:27:05 +1000

    testing pull request for the ci and cd dom

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="about">
     <h1>
-      This is pull request! 
-      Please review it and merge to the main/master branch 
-    </h1>
-  </div>
+      This is pull request!          
 </template>
