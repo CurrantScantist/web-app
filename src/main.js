f2f92a260ac8ea435e72f16f045ec3775a484f00
dpham06 <dpha0005@student.monash.edu>
2021-07-24 01:30:50 +1000

    feat: add element-plus components, add global css styling

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// styles
+import "@/styles/admin.scss";
+
+// Element Plus
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
