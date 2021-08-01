dd8029ca751bf94e54a77174d2c1b9cc0a81d615
shourya <sraj0008@student.monash.edu>
2021-08-01 16:43:01 +1000

    fix: the location of directory

--- FastAPI-mongoDB/app/test/teckstack_test.py
+++ FastAPI-mongoDB/app/test/teckstack_test.py
@@ -1,7 +1,7 @@
 # Mounting the directory to app
 import os
 import sys
-sys.path.insert(0, os.getcwd())
+sys.path.insert(0, os.getcwd()+"/FastAPI-mongoDB/app")
 print(os.getcwd())
 from server.database import connect
 from dotenv import load_dotenv
