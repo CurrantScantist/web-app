9f4126c4b697bb48b8dd1514f41bcdb87b6a88b4
dpham06 <dpha0005@student.monash.edu>
2021-08-02 17:35:45 +1000

    refactor: update routes

--- src/router/other-routes.js
+++ src/router/other-routes.js
@@ -1,1 +1,12 @@
-export default [];
+import Landing from "@/views/Landing";
+
+export default [
+  {
+    path: "/",
+    name: "landing",
+    component: Landing,
+    meta: {
+      icon: "",
+    },
+  },
+];
