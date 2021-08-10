54d5861e6b360e2e000eb4c7ad9486df452405b5
Om Mandavia <oman0003@student.monash.edu>
2021-08-11 01:26:35 +1000

    feature: Made charts Dynamic to dataset

--- src/views/OneOneRepoComparison.vue
+++ src/views/OneOneRepoComparison.vue
@@ -12,44 +12,44 @@
                     </span> 
 
                     <h1>
-                        {{repository1.owner}}/{{repository1.name}}
+                        {{repository1MetaData.owner}}/{{repository1MetaData.name}}
                     </h1>
                     
                     <h5>
-                        {{repository1.description}}
+                        {{repository1MetaData.description}}
                     </h5>
 
                     <div class= "info-grid">
                         <div class="info-item">
-                            <div class="stat-name">Forks Count:</div><div>{{(repository1.forks_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Forks Count:</div><div>{{(repository1MetaData.forks_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Forks: </div><div>{{(repository1.forks || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Forks: </div><div>{{(repository1MetaData.forks || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Watchers: </div><div>{{(repository1.watchers_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Watchers: </div><div>{{(repository1MetaData.watchers_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Issues: </div><div>{{(repository1.open_issues_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Issues: </div><div>{{(repository1MetaData.open_issues_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Tags: </div><div>{{(repository1.topics || []).join(', ')}}</div>
+                            <div class="stat-name">Tags: </div><div>{{(repository1MetaData.topics || []).join(', ')}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Default Branch: </div><div>{{repository1.default_branch}}</div>
+                            <div class="stat-name">Default Branch: </div><div>{{repository1MetaData.default_branch}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Stargazers: </div><div>{{(repository1.stargazers_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Stargazers: </div><div>{{(repository1MetaData.stargazers_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Project Size: </div><div>{{(repository1.size || 0).toLocaleString()}} bytes</div>
+                            <div class="stat-name">Project Size: </div><div>{{(repository1MetaData.size || 0).toLocaleString()}} bytes</div>
                         </div>
 
                     </div>
@@ -68,13 +68,13 @@
                         <div class="wide-visualisation1">
                             <h3>Lines of Code by language</h3>
                             <h6>(over versions in %)</h6>
-                            <v-echarts v-bind:option="option3" style="height: 500px" />
+                            <v-echarts v-bind:option="locLang1" style="height: 500px" />
                         </div>
 
                         <div class="wide-visualisation2">
                             <h3>Lines in files by type</h3>
                             <h6>(in %)</h6>
-                            <v-echarts v-bind:option="option4" style="height: 500px" />
+                            <v-echarts v-bind:option="locType1" style="height: 500px" />
                         </div>
                     </div>
                 </div>
@@ -91,44 +91,44 @@
                     </span> 
 
                     <h1>
-                        {{repository2.owner}}/{{repository2.name}}
+                        {{repository2MetaData.owner}}/{{repository2MetaData.name}}
                     </h1>
                     
                     <h5>
-                        {{repository2.description}}
+                        {{repository2MetaData.description}}
                     </h5>
 
                     <div class= "info-grid">
                         <div class="info-item">
-                            <div class="stat-name">Forks Count:</div><div>{{(repository2.forks_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Forks Count:</div><div>{{(repository2MetaData.forks_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Forks: </div><div>{{(repository2.forks || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Forks: </div><div>{{(repository2MetaData.forks || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Watchers: </div><div>{{(repository2.watchers_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Watchers: </div><div>{{(repository2MetaData.watchers_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Issues: </div><div>{{(repository2.open_issues_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Issues: </div><div>{{(repository2MetaData.open_issues_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Tags: </div><div>{{(repository2.topics || []).join(', ')}}</div>
+                            <div class="stat-name">Tags: </div><div>{{(repository2MetaData.topics || []).join(', ')}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Default Branch: </div><div>{{repository2.default_branch}}</div>
+                            <div class="stat-name">Default Branch: </div><div>{{repository2MetaData.default_branch}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Stargazers: </div><div>{{(repository2.stargazers_count || 0).toLocaleString()}}</div>
+                            <div class="stat-name">Stargazers: </div><div>{{(repository2MetaData.stargazers_count || 0).toLocaleString()}}</div>
                         </div>
 
                         <div class="info-item">
-                            <div class="stat-name">Project Size: </div><div>{{(repository2.size || 0).toLocaleString()}} bytes</div>
+                            <div class="stat-name">Project Size: </div><div>{{(repository2MetaData.size || 0).toLocaleString()}} bytes</div>
                         </div>
 
                     </div>
@@ -147,13 +147,13 @@
                         <div class="wide-visualisation1">
                             <h3>Lines of Code by language</h3>
                             <h6>(over versions in %)</h6>
-                            <v-echarts v-bind:option="option3" style="height: 500px" />
+                            <v-echarts v-bind:option="locLang2" style="height: 500px" />
                         </div>
 
                         <div class="wide-visualisation2">
                             <h3>Lines in files by type</h3>
                             <h6>(in %)</h6>
-                            <v-echarts v-bind:option="option4" style="height: 500px" />
+                            <v-echarts v-bind:option="locType2" style="height: 500px" />
                         </div>
                     </div>
                 </div>
@@ -169,6 +169,8 @@
 
     import { VEcharts } from 'vue3-echarts';
     import locByType from '@/visualisations/LinesOfCodeByType.json'
+    import locByLang from '@/visualisations/LinesOfCodeByLanguage.json'
+    import seriesObj from '@/visualisations/SeriesSubObjLangLOC.json'
     
     export default {
     name: 'OneOneRepoComparison',
@@ -177,10 +179,14 @@
     } ,
      data () {
       return {
-            repository1: {},
-            repository2: {}, 
-            fetchURL1: "https://scantist-backend.herokuapp.com/techstack/{name_owner}?name=flask&owner=pallets",
-            fetchURL2: "https://scantist-backend.herokuapp.com/techstack/{name_owner}?name=grab&owner=lorien",
+            repository1MetaData: {},
+            repository2MetaData: {}, 
+            repository1Stats: {},
+            repository2Stats: {},
+            fetchMetaData1URL: "https://scantist-backend.herokuapp.com/techstack/{name_owner}?name=flask&owner=pallets",
+            fetchMetaData2URL: "https://scantist-backend.herokuapp.com/techstack/{name_owner}?name=grab&owner=lorien",
+            fetchStatsData1URL: "https://run.mocky.io/v3/28c55b76-6424-470e-9440-a164ee8aed09",
+            fetchStatsData2URL: "https://run.mocky.io/v3/4c239fe2-a2cc-4837-a8ac-039f20be3aae",
             option2: {
                 tooltip: {
                     trigger: 'item'
@@ -209,71 +215,131 @@
                     }
                 ]
             },
-            option3:  {},
-            option4: {}
+            locLang1: {},
+            locLang2: {},
+            locType1: {},
+            locType2: {}
        }
     },
     components: {
         VEcharts,
     },
     methods: {
-        getColor() {
+        getColor(size) {
             let colorSet = ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00','#ff5100', '#8800ff', '#82173f', '#383838', '#000000']
-            console.log(colorSet)
+            let colorPalette = []
+            for (let i = 0; i < size; i++) {
+                if (i>=colorSet.length) {
+                    colorPalette.push(colorSet[i%colorSet.length])
+                } else {
+                    colorPalette.push(colorSet[i])
+                }
+            }
+            return colorPalette
         },
         async fetchData() {
-            const repo1Fetch = await fetch("https://cors-anywhere.herokuapp.com/"+this.fetchURL1, {
+            const repo1MetaDataFetch = await fetch("https://cors-anywhere.herokuapp.com/"+this.fetchMetaData1URL, {
             method: "GET",
             headers: {
                 'content-type': 'application/json'
             }
-        })
-        const repo2Fetch = await fetch("https://cors-anywhere.herokuapp.com/"+this.fetchURL2, {
-            method: "GET",
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
+            })
+            
+            const repo2MetaDataFetch = await fetch("https://cors-anywhere.herokuapp.com/"+this.fetchMetaData2URL, {
+                method: "GET",
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
 
-        // lorien/grab: https://run.mocky.io/v3/4c239fe2-a2cc-4837-a8ac-039f20be3aae
-        // pallets/flask: https://run.mocky.io/v3/28c55b76-6424-470e-9440-a164ee8aed09
-        // pallets/flask delete: https://designer.mocky.io/manage/delete/28c55b76-6424-470e-9440-a164ee8aed09/fit4002
-        // lorien/grab delete: https://designer.mocky.io/manage/delete/4c239fe2-a2cc-4837-a8ac-039f20be3aae/fit4002
+            const responseMetaDataRepo1 = await repo1MetaDataFetch.json()
+            const responseMetaDataRepo2 = await repo2MetaDataFetch.json()
+            this.repository1MetaData = responseMetaDataRepo1.data[0]
+            this.repository2MetaData = responseMetaDataRepo2.data[0]
 
+            const repo1StatsFetch = await fetch(this.fetchStatsData1URL, {
+                method: "GET",
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            const repo2StatsFetch = await fetch(this.fetchStatsData2URL, {
+                method: "GET",
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            
+            this.repository1Stats = await repo1StatsFetch.json()
+            this.repository2Stats= await repo2StatsFetch.json()
 
-        const responseDataRepo1 = await repo1Fetch.json()
-        const responseDataRepo2 = await repo2Fetch.json()
-        this.repository1 = responseDataRepo1.data[0]
-        this.repository2 = responseDataRepo2.data[0]
-        setTimeout(()=>{
-          this.option4 = this.processData(locByType)
-        }, 3000)
-
-        setTimeout(()=>{
-          this.option3 = this.processData(locByType)
-        }, 9000)
+            this.processData(1)
+            this.processData(2)
+            
         },
-        processData(obj){
-            let copyObj = obj
+        setSeriesSubObject(chart,map) {
+            map.forEach((value, key) => {
+                let seriesSubObjCopy = JSON.parse(JSON.stringify(seriesObj))
+                seriesSubObjCopy.name = key
+                seriesSubObjCopy.areaStyle.color = value.colors
+                seriesSubObjCopy.data = value.data
+                chart.series.push(seriesSubObjCopy)
+            })
+        },
+        assignColor(givenMap,colorPalette){
+            let colorAllocationIndex = 0
+            givenMap.forEach((value, key, map) =>{
+                let tempValue = value
+                tempValue.color = colorPalette[colorAllocationIndex++]
+                map.set(key, tempValue)
+            })
+        },
+        processData(repoNumber){
+            let locByLangCopy = JSON.parse(JSON.stringify(locByLang))
+            let locByTypeCopy = JSON.parse(JSON.stringify(locByType))
+            const languageData = new Map();
+            const statsData = new Map();
             
             let versions = ["V1.1.0","V1.1.1","V1.1.2","V1.2.0","V1.3.0","V1.4.0","V1.5.0","V1.5.2"]
-            let codeLines = [0.7, 0.74, 0.67, 0.66, 0.65, 0.67, 0.64, 0.67]
-            let commentLines = [0.2, 0.18, 0.23, 0.23, 0.25, 0.23, 0.25, 0.25]
-            let blankLines = [0.1, 0.080, 0.1, 0.11, 0.1, 0.1, 0.11, 0.080]
+            statsData.set("Code", {"data":[0.7, 0.74, 0.67, 0.66, 0.65, 0.67, 0.64, 0.67], "color": "#000000"})
+            statsData.set("Comments", {"data": [0.2, 0.18, 0.23, 0.23, 0.25, 0.23, 0.25, 0.25], "color": "#383838"})
+            statsData.set("Blank", {"data":[0.1, 0.080, 0.1, 0.11, 0.1, 0.1, 0.11, 0.080], "color": "#8a8888"})
 
-            copyObj.xAxis[0].data = versions
-            copyObj.series[0].data = codeLines
-            copyObj.series[1].data = commentLines
-            copyObj.series[2].data = blankLines
+            languageData.set("Java", {"data":[0.4, 0.425, 0.5, 0.53, 0.51, 0.55, 0.31, 0.2]})
+            languageData.set("JavaScript", {"data": [0.3, 0.3, 0.28, 0.2, 0.2, 0.2, 0.38, 0.51]})
+            languageData.set("CSS", {"data": [0.05, 0.1, 0.15, 0.09, 0.2, 0.13, 0.11, 0.2]})
+            languageData.set("HTML", {"data": [0.075, 0.05, 0.02, 0.18, 0.09, 0.12, 0.2, 0.09]})
+            languageData.set("Python", {"data": [0.175, 0.125, 0.05, 0, 0, 0, 0, 0]})
+        
+            let colorPalette = this.getColor(languageData.size)
+            this.assignColor(languageData,colorPalette)
 
-            return copyObj
-        },
-        changeLoadingAnimation(){
+            locByLangCopy.xAxis[0].data = versions
+            locByTypeCopy.xAxis[0].data = versions
 
+            locByLangCopy.color = colorPalette
+
+            locByLangCopy.legend.data = Array.from(languageData.keys())
+            locByTypeCopy.legend.data = Array.from(statsData.keys())
+
+            this.setSeriesSubObject(locByLangCopy,languageData)
+            this.setSeriesSubObject(locByTypeCopy,statsData)
+
+            if (repoNumber == 1) {
+                this.locType1 = locByTypeCopy
+                this.locLang1 = locByLangCopy
+            } else if (repoNumber == 2) {
+                this.locType2 = locByTypeCopy
+                this.locLang2 = locByLangCopy
+            }     
+            
         }
     }
 }
 
-    
-    
+// lorien/grab: https://run.mocky.io/v3/4c239fe2-a2cc-4837-a8ac-039f20be3aae
+// pallets/flask: https://run.mocky.io/v3/28c55b76-6424-470e-9440-a164ee8aed09
+// pallets/flask delete: https://designer.mocky.io/manage/delete/28c55b76-6424-470e-9440-a164ee8aed09/fit4002
+// lorien/grab delete: https://designer.mocky.io/manage/delete/4c239fe2-a2cc-4837-a8ac-039f20be3aae/fit4002
+
 </script>
