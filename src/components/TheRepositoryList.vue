abcdbb1f2af85b5d413e66bf78a6615eafde29c6
dpham06 <dpha0005@student.monash.edu>
2021-10-03 03:58:27 +1100

    feat: add function to convert numbers into a reader friendly format

--- src/components/TheRepositoryList.vue
+++ src/components/TheRepositoryList.vue
@@ -27,14 +27,21 @@
             </el-table-column>
             <el-table-column property="name" label="Repository">
             </el-table-column>
-            <el-table-column
-              property="owner"
-              label="Owner"
-              show-overflow-tooltip
-            >
+            <el-table-column property="forks">
+              <template #header>
+                <span class="table-header">Forks</span>
+              </template>
+              <template #default="scope">
+                <span>{{ abbrNum(scope.row.forks, 1) }}</span>
+              </template>
             </el-table-column>
-            <el-table-column property="forks" label="Forks"> </el-table-column>
-            <el-table-column property="stargazers_count" label="Stargazers">
+            <el-table-column property="stargazers_count">
+              <template #header>
+                <span class="table-header">Stargazers</span>
+              </template>
+              <template #default="scope">
+                <span>{{ abbrNum(scope.row.stargazers_count, 1) }}</span>
+              </template>
             </el-table-column>
             <el-table-column
               property="topics"
@@ -229,6 +236,49 @@ export default defineComponent({
     },
     printMessage(message) {
       this.$message.error(message);
+    },
+    /**
+     * Converts a number to a reader friendly format e.g. 1234 to 1.2K
+     * @see https://stackoverflow.com/questions/2685911/is-there-a-way-to-round-numbers-into-a-reader-friendly-format-e-g-1-1k
+     *
+     * @param {number} number - The number to be converted.
+     * @param {number} decPlaces - The number of decimal places to round to.
+     *
+     * @return {string} A number in a reader friendly format.
+     */
+    abbrNum(number, decPlaces) {
+      // 2 decimal places => 100, 3 => 1000, etc
+      decPlaces = Math.pow(10, decPlaces);
+
+      // Enumerate number abbreviations
+      var abbrev = ["K", "M", "B", "T"];
+
+      // Go through the array backwards, so we do the largest first
+      for (var i = abbrev.length - 1; i >= 0; i--) {
+        // Convert array index to "1000", "1000000", etc
+        var size = Math.pow(10, (i + 1) * 3);
+
+        // If the number is bigger or equal do the abbreviation
+        if (size <= number) {
+          // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+          // This gives us nice rounding to a particular decimal place.
+          number = Math.round((number * decPlaces) / size) / decPlaces;
+
+          // Handle special case where we round up to the next abbreviation
+          if (number == 1000 && i < abbrev.length - 1) {
+            number = 1;
+            i++;
+          }
+
+          // Add the letter for the abbreviation
+          number += abbrev[i];
+
+          // We are done... stop
+          break;
+        }
+      }
+
+      return number;
     },
   },
 });
