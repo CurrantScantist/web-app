1fc487f87890da837ed6f92a43418ac64313f978
shourya <sraj0008@student.monash.edu>
2021-10-15 04:04:04 +1100

    test: Added more tests to test API endpoints

--- tests/e2e/specs/api_integration/repository_list_page.js
+++ tests/e2e/specs/api_integration/repository_list_page.js
@@ -1,6 +1,6 @@
 // Testing /repositories page
 
-describe("Testing Load of /repositories page", () => {
+describe("Testing on Load of /repositories page", () => {
   it("Checking Input placeholder string match", () => {
     cy.visit("/repositories");
     cy.get("input")
@@ -10,17 +10,17 @@ describe("Testing Load of /repositories page", () => {
 });
 
 // Testing backend call from repository page using mock data
-describe("Backend Mock Endpoint for top ten teckstack", () => {
+describe("Backend Mock Endpoint for list of teckstack", () => {
   beforeEach("testing techstack/list endpoint", () => {
     cy.server();
     cy.route("GET", "**/techstack/list**", "fixture:techstack_list.json");
   });
 
-  it("Checking whether mock data populated in /repositories page", () => {
+  it("Checking whether mock data is getting populated in /repositories page", () => {
     cy.visit("/repositories");
     // Testing repository name
     cy.get("td").contains("shadowsocks-windows");
-    // Testing ownwer
+    // Testing owner
     cy.get("td").contains("beetbox");
     // Testing fork count
     cy.get("td").contains(91);
